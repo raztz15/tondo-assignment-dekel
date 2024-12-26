0b9018ad4e13ec7ddd1ce662730ad9d030a6dcbf
raztz15 <raztz15@gmail.com>
2024-12-26 18:14:45 +0200

    added base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tondo-assignment-dekel'
 })
